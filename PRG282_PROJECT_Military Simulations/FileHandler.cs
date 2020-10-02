ef2f327bc9ea02225f77367c7620dd6f2c50644a
SibahleMayekiso <mayekisosibahle@gmail.com>
2020-10-02 17:52:13 +0200

    Added FileHandler class as well as the Report Class

---
+++ PRG282_PROJECT_Military Simulations/FileHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PRG282_PROJECT
+{
+    
+    class FileNotExistException: ApplicationException //This is the custom exeption class
+    {
+        public void NoFileException()
+        {
+            MessageBox.Show("Error! The file could not be connected to. Please check to see if the file exists");
+        }
+    }
+    class FileHandler
+    {
+        SqlConnection conn = new SqlConnection("Server=.; Initial Catalog=MilitaryDB; Integrated Security=SSPI");
+        
+        public void openConnection()
+        {
+            //Below is the implementation of our custom exception "FileNotExistException"
+            try
+            {
+                conn.Open();
+            }
+            catch (FileNotExistException ex)
+            {
+
+                ex.NoFileException();
+            }
+        }
+
+        //List<Aircraft> lstAircraft = new List<Aircraft>();
+
+        //This method will be used to get the data from the database and store it into 
+        // a DataTable
+        //public void getAircraftData()
+        //{
+        //    string query = @"SELECT * FROM Aircraft";
+
+        //    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+        //    DataTable table = new DataTable();
+
+        //    adapter.Fill(table);
+
+        //    //Here we are adding contents of the DataTable into a list ("lstAircraft")
+        //    foreach (DataRow row in table.Rows)
+        //    {
+        //        lstAircraft.Add(new Aircraft(row["ID"], row["AircraftName"], row["Model"], row["FuelTankSize"], row["TopSpeed"], row["Health"], row["Weapon"], row["Ammunition"], row["DamageScale"], row["Quantity"]));
+        //    }
+        //}
+
+        
+        
+    }
+}
---
+++ PRG282_PROJECT_Military Simulations/Report.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG282_PROJECT_Military_Simulations
+{
+    class Report
+    {
+        //use this method when the gameover event is triggered
+        private static string generateReport(int aircraftHealth, int fuel, int enemCampHealth, int ammo, int score)
+        {
+            string FinalAnalysis;
+
+            string AllyHealth;
+            string FuelStatus;
+            string EnemyHealth;
+            string Inventory;
+            string Damage;
+
+
+            if (aircraftHealth > 70)
+            {
+                AllyHealth = "Current Aircraft Health:" + aircraftHealth + "\n Aircraft is in great condition";
+            }
+            else if(aircraftHealth > 25 && aircraftHealth < 40)
+            {
+                AllyHealth = "Current Aircraft Health:" + aircraftHealth + "\n Aircraft has suffered considerable damage";
+            }
+            else
+            {
+                AllyHealth = "Current Aircraft Health:" + aircraftHealth + "\n Aircraft is in critical condition";
+            }
+            
+            //Different criteria can be assignd using if statement like the code  above 
+            FuelStatus = "Current Fuel:" + fuel;
+            EnemyHealth = "Current Enemy Camp Health: " + aircraftHealth;
+            Inventory = "Current Ammunition available: " + ammo;
+            Damage = "Current Damage inflicted on enemy: " + score;
+            ////
+
+            //Condition checking to determine final analysis
+            if (aircraftHealth > 75 && enemCampHealth < 1 && score > 1)
+            {
+                FinalAnalysis = "Mission Was Very Successfull.\n Enemy suffered multiple casualities with minimal reporcusions for Aircraft";
+            }
+            else if(aircraftHealth > 50 && enemCampHealth < 1 && score > 1)
+            {
+                FinalAnalysis = "Mission Was Very Successfull.\n Enemy suffered multiple casualities with moderate reporcusions for Aircraft";
+            }
+            else if (aircraftHealth > 25 && enemCampHealth < 1 && score > 1)
+            {
+                FinalAnalysis = "Mission Was Very Successfull.\n Enemy suffered multiple casualities with fairly heavy reporcusions for Aircraft";
+            }
+            else if (aircraftHealth > 1)
+            {
+                FinalAnalysis = "Mission Was Unsuccessful";
+            }
+            else
+            {
+                FinalAnalysis = "Mission Was Unsuccessful";
+            }
+
+
+            return AllyHealth + "\n" + FuelStatus + "\n" + EnemyHealth + "\n" + Inventory + "\n" + Damage + "\n" + FinalAnalysis;
+        }
+    }
+}
